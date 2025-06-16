e8dc7bfce6eb95746264ebdde9b1f2db2ad6e5f7
Ranita-Magar <ranitamagar10@gmail.com>
2025-06-16 13:32:29 +0545

    updated cart icon to display item count dynamically

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -59,6 +59,27 @@ export const AppContextProvider = ({ children }) => {
     fetchProducts();
   }, []);
 
+  //get cart item count
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  };
+
+  //get cart total amount
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      if (cartItems[items] > 0) {
+        totalAmount += itemInfo.offerPrice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  };
+
   const value = {
     navigate,
     user,
@@ -75,6 +96,8 @@ export const AppContextProvider = ({ children }) => {
     cartItems,
     searchQuery,
     setSearchQuery,
+    getCartCount,
+    getCartAmount,
   };
 
   return <AppContext.Provider value={value}>{children} </AppContext.Provider>;
