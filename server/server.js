a00e5b1f16170dd6ae55cc2c72fa27f1d62d129b
Ranita-Magar <ranitamagar10@gmail.com>
2025-06-17 11:31:15 +0545

    Created AuthSeller and SellerLogout routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import "dotenv/config";
 import userRouter from "./routes/UserRoute.js";
+import sellerRouter from "./routes/SellerRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -23,6 +24,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/seller", sellerRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
